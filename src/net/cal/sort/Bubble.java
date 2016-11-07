2d883ecc6bfbae3f12de128096342866e50d7164
wangxm <wangxm@zhen.com>
2016-11-07 17:28:20 +0800

    初始化
    Change-Id: Iae80cc2871c3dd7188adb19e5f48c36a13e2f05f
    Signed-off-by: wangxm <wangxm@zhen.com>

---
+++ src/net/cal/sort/Bubble.java
@@ -0,0 +1,31 @@
+package net.cal.sort;
+
+/**
+ * 冒泡
+ * 
+ * @version 0.1
+ * @author wangxm
+ *
+ */
+public class Bubble {
+    public static void main(String[] args) {
+        int[] intarr = new int[] {
+            1, -2, 0, -1
+        };
+
+        for (int i = 0; i < intarr.length; i++) {
+            for (int j = 0; j < intarr.length; j++) {
+                if (intarr[i] < intarr[j]) {
+                    int tmp = intarr[i];
+                    intarr[i] = intarr[j];
+                    intarr[j] = tmp;
+                    System.out.println("####################");
+                    for (int x : intarr) {
+                        System.out.println(x);
+                    }
+                }
+            }
+        }
+
+    }
+}
